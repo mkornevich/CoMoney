72b87187dc267b5f1b26ae6b6e33bdef9a2e16da
mkornevich <mkornevich3@gmail.com>
2021-05-16 22:41:32 +0300

    add payment list functionality

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -11,6 +11,7 @@ use App\Repository\PaymentRepository;
 use App\Updater\CampaignTotalAmountUpdater;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +21,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PaymentController extends AbstractController
 {
+    /**
+     * @Route("/payment/list", name="payment_list")
+     */
+    public function list(Request $request, PaymentRepository $paymentRepository, PaginatorInterface $paginator): Response
+    {
+        $paymentQB = $paymentRepository->getAllEagerByUserQB($this->getUser());
+        $query = $paymentQB->getQuery();
+        $pagination = $paginator->paginate($query, $request->query->getInt('page', 1), 20);
+
+        return $this->render('payment/list.html.twig', [
+            'payments' => $query->execute(),
+            'pagination' => $pagination,
+        ]);
+    }
+
     /**
      * @Route("/campaign/{campaign_id<\d+>}/pay", name="payment_pay")
      * @Entity("campaign", expr="repository.find(campaign_id)")
--- src/Repository/PaymentRepository.php
+++ src/Repository/PaymentRepository.php
@@ -4,8 +4,11 @@
 namespace App\Repository;
 
 
+use App\Entity\Campaign;
 use App\Entity\Payment;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 class PaymentRepository extends ServiceEntityRepository
@@ -19,5 +22,22 @@ class PaymentRepository extends ServiceEntityRepository
     {
         $this->_em->persist($payment);
         $this->_em->flush();
+    }
+
+    public function getAllEagerQB(): QueryBuilder
+    {
+        return $this->createQueryBuilder('payment')
+            ->addSelect(['campaign', 'user'])
+            ->leftJoin('payment.campaign', 'campaign')
+            ->leftJoin('payment.user', 'user')
+            ->orderBy('payment.createdAt', 'DESC');
+    }
+
+    public function getAllEagerByUserQB(User $user): QueryBuilder
+    {
+        return $this->getAllEagerQB()
+            ->where('user.id = :user_id')
+            ->setParameter('user_id', $user->getId())
+    ;
     }
 }
